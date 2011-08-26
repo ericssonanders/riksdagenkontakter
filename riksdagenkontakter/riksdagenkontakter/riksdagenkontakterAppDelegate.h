4d5e5151b2cbef180be911ce95979c94f8fa99a7
Anders Ericsson <ericssonanders@gmail.com>
2011-08-26 22:31:08 +0200

    added project files

---
+++ riksdagenkontakter/riksdagenkontakter/riksdagenkontakterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  riksdagenkontakterAppDelegate.h
+//  riksdagenkontakter
+//
+//  Created by Anders Ericsson on 2011-08-26.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface riksdagenkontakterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
